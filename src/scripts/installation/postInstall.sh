70d472fdada6ca748332da65a9827e11a2c3d1ae
ksetetemela <khobatha.setetemela@gmail.com>
2021-03-17 03:23:50 +0200

    added missing config dirs and dhis2_log table

--- src/scripts/installation/postInstall.sh
+++ src/scripts/installation/postInstall.sh
@@ -15,13 +15,31 @@ if [ $? -eq 1 ]; then
     useradd -g ${USER} ${USER}
 fi
 
+mysql --user="root" --password="P@ssw0rd" --database="openmrs" --execute="CREATE table dhis2_log ( 
+																		id INT(6) unsigned auto_increment primary key, 
+																		report_name varchar(30) not null, 
+																		submitted_date timestamp, 
+																		submitted_by varchar(30) not null, 
+																		report_log varchar(1000) not null, 
+																		status varchar(30) not null,
+																		comment varchar(30) not null, 
+																		report_month integer, 
+																		report_year integer);"
+
+
 usermod -s /usr/sbin/nologin bahmni
 
 mkdir -p /opt/dhis-integration/var/log/
 mkdir /etc/dhis-integration/
 mkdir /var/log/dhis-integration/
+mkdir /dhis-integration/
+mkdir /dhis-integration-data/
+mkdir /var/www/bahmni_config/dhis2/
 
 chown -R bahmni:bahmni /opt/dhis-integration/
+chown -R bahmni:bahmni /dhis-integration/
+chown -R bahmni:bahmni /dhis-integration-data/
+chown -R bahmni:bahmni /var/www/bahmni_config/dhis2/ 
 chmod +x /opt/dhis-integration/bin/dhis-integration
 
 mv /opt/dhis-integration/bin/dhis-integration*.jar /opt/dhis-integration/bin/dhis-integration.jar
@@ -30,5 +48,7 @@ ln -sf /opt/dhis-integration/etc/application.yml /etc/dhis-integration/dhis-inte
 ln -sf /opt/dhis-integration/etc/log4j.properties /etc/dhis-integration/log4j.properties
 ln -sf /opt/dhis-integration/var/log/dhis-integration.log /var/log/dhis-integration/dhis-integration.log
 ln -sf /opt/dhis-integration/bin/dhis-integration /etc/init.d/dhis-integration
+
+
 
 chkconfig --add dhis-integration
