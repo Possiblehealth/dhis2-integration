631b308a0ef8d3cbff3ec9ea7db2b02d62e2b25d
ksetetemela <khobatha.setetemela@gmail.com>
2021-05-31 00:35:51 +0200

    Update init.js

--- src/main/resources/static/js/init.js
+++ src/main/resources/static/js/init.js
@@ -219,14 +219,14 @@ function putStatusRefresh(data, index) {
 		//alert("[putStatus] Status is SUCCESS...updating...displaying the data");
 		//alert(data.status);
 		reportStatus = 'Success';
-		var template = $('#success-refresh-status-template').html();
+		var template = $('#success-status-template').html();
 		Mustache.parse(template);
 		element('status', index).html(Mustache.render(template, reportStatus));
 		return;
 	}
 	//alert("[putStatus] Status is FAILURE...updating...displaying the data");
 	//alert(data.status.status);
-	var template = $('#failure-refresh-status-template').html();
+	var template = $('#failure-status-template').html();
 	Mustache.parse(template);
 	data.message = JSON.stringify(data.exception || data.response);
 	element('status', index).html(Mustache.render(template, reportStatus));
