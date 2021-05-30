a9ca5b35a1e93d41936b924f3932034a79ef64ac
ksetetemela <khobatha.setetemela@gmail.com>
2021-05-30 23:12:15 +0200

    Update init.js

--- src/main/resources/static/js/init.js
+++ src/main/resources/static/js/init.js
@@ -218,7 +218,7 @@ function putStatusRefresh(data, index) {
 		//alert(data.status);
 		var template = $('#success-refresh-status-template').html();
 		Mustache.parse(template);
-		element('status', index).html(Mustache.render(template, data.response));
+		element('status', index).html(Mustache.render(template, data.response.status));
 		return;
 	}
 	//alert("[putStatus] Status is FAILURE...updating...displaying the data");
