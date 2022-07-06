a121df8aa9b84c8d1e48e207dbf5980b67db6348
ksetetemela <khobatha.setetemela@gmail.com>
2022-07-06 15:53:10 +0200

    security fixes

---
+++ src/main/resources/static/js/init2.js
@@ -0,0 +1,129 @@
+var reportConfigUrl = '/bahmni_config/openmrs/apps/reports/reports.json';
+var downloadUrl = '/dhis-integration/download?name=NAME&year=YEAR&month=MONTH&isImam=IS_IMAM&isFamily=IS_FAMILY';
+var submitUrl = '/dhis-integration/submit-to-dhis';
+var submitUrlAtr = '/dhis-integration/submit-to-dhis-atr';
+var loginRedirectUrl = '/bahmni/home/index.html#/login?showLoginMessage&from=';
+var NUTRITION_PROGRAM = '03-2 Nutrition Acute Malnutrition';
+var FAMILYPLANNING_PROGRAM = '07 Family Planning Program';
+var logUrl = '/dhis-integration/log';
+var fiscalYearReportUrl = '/dhis-integration/download/fiscal-year-report?name=NAME&startYear=START_YEAR&startMonth=START_MONTH&endYear=END_YEAR&endMonth=END_MONTH&isImam=IS_IMAM';
+var supportedStartDate = 2033;
+var supportedEndDate = 2008;
+var approximateNepaliYear = (new Date()).getFullYear() + 56;
+var spinner = spinner || {};
+
+var months = [ {
+	number : 12,
+	name : "December"
+}, {
+	number : 11,
+	name : "November"
+}, {
+	number : 10,
+	name : "October"
+}, {
+	number : 9,
+	name : "September"
+}, {
+	number : 8,
+	name : "August"
+}, {
+	number : 7,
+	name : "July"
+}, {
+	number : 6,
+	name : "June"
+}, {
+	number : 5,
+	name : "May"
+}, {
+	number : 4,
+	name : "April"
+}, {
+	number : 3,
+	name : "March"
+}, {
+	number : 2,
+	name : "February"
+}, {
+	number : 1,
+	name : "January"
+} ];
+
+var years = range(supportedStartDate, supportedEndDate);
+var fiscalYears = fiscalYearRange(supportedStartDate, supportedEndDate);
+var hasReportingPrivilege = true;
+
+$(document).ready(
+		function() {
+
+			// Activate tooltip
+			$('[data-toggle="tooltip"]').tooltip();
+			
+			// Select/Deselect checkboxes
+			var checkbox = $('table tbody input[type="checkbox"]');
+			$("#selectAll").click(function(){
+				if(this.checked){
+					checkbox.each(function(){
+						this.checked = true;                        
+					});
+				} else{
+					checkbox.each(function(){
+						this.checked = false;                        
+					});
+				} 
+			});
+			checkbox.click(function(){
+				if(!this.checked){
+					$("#selectAll").prop("checked", false);
+				}
+			});
+
+			initTabs();
+
+		});
+
+
+
+
+
+function initTabs() {
+	$("#tabs").tabs();
+}
+
+
+function element(name, index) {
+	var id = name + '-' + index;
+	return $('[id="' + id + '"]');
+}
+
+function enableBtn(btn) {
+	return btn.attr('disabled', false).removeClass('btn-disabled');
+}
+
+function disableBtn(btn) {
+	return btn.attr('disabled', true).addClass('btn-disabled');
+}
+
+function disableAllSubmitBtns() {
+	disableBtn($("[id*='submit-']"));
+}
+
+function registerOnchangeOnComment() {
+	disableAllSubmitBtns();
+	$("[id*='comment-']").on('change keyup paste', function(event) {
+		var index = $(event.target).attr('index');
+		if ($(event.target).val().trim() != "") {
+			enableBtn(element('submit', index));
+		} else {
+			disableBtn(element('submit', index));
+		}
+	});
+}
+
+function getLogStatus() {
+	$('#programs .month-selector').each(function(index) {
+		getStatus(index);
+	});
+}
+
