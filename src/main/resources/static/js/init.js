7f56374058e416802cb82f5ab778e91963d04921
ksetetemela <khobatha.setetemela@gmail.com>
2022-07-06 13:41:19 +0200

    fixes

--- src/main/resources/static/js/init.js
+++ src/main/resources/static/js/init.js
@@ -56,6 +56,29 @@ var hasReportingPrivilege = true;
 
 $(document).ready(
 		function() {
+
+			// Activate tooltip
+			$('[data-toggle="tooltip"]').tooltip();
+			
+			// Select/Deselect checkboxes
+			var checkbox = $('table tbody input[type="checkbox"]');
+			$("#selectAll").click(function(){
+				if(this.checked){
+					checkbox.each(function(){
+						this.checked = true;                        
+					});
+				} else{
+					checkbox.each(function(){
+						this.checked = false;                        
+					});
+				} 
+			});
+			checkbox.click(function(){
+				if(!this.checked){
+					$("#selectAll").prop("checked", false);
+				}
+			});
+
 			isAuthenticated().then(isSubmitAuthorized).then(initTabs).then(
 				renderPrograms).then(renderYearlyReport).then(
 				selectApproxLatestGregorianYear).then(
