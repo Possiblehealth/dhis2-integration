8a674c12264d6c69b1ee008753a76b07456db4ec
ksetetemela <khobatha.setetemela@gmail.com>
2022-09-03 20:53:04 +0200

    update reinstall.sh

--- src/scripts/debug/reinstall.sh
+++ src/scripts/debug/reinstall.sh
@@ -4,6 +4,6 @@ cd ../../../
 sudo yum autoremove dhis-integration -y
 mvn -Dmaven.test.skip=true install
 sudo yum install target/rpm/dhis-integration/RPMS/noarch/dhis-integration-1.0-1.noarch.rpm -y
-cd ~
-sudo cp application.yml /opt/dhis-integration/
+cd $HOME
+sudo cp application.yml /opt/dhis-integration/etc
 sudo systemctl restart dhis-integration
