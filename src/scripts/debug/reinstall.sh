ea053314a9418b8cb7b409c4029f178d23ea3945
ksetetemela <khobatha.setetemela@gmail.com>
2022-09-03 21:14:08 +0200

    update reinstall.sh

--- src/scripts/debug/reinstall.sh
+++ src/scripts/debug/reinstall.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 DHISIntegratorHome=/development/bahmni-app-dhis2-integration/
 
-cd ${DHISIntegratorHome}
-git pull
+cd /development/bahmni-app-dhis2-integration/
 sudo yum autoremove dhis-integration -y
 mvn -Dmaven.test.skip=true install
 sudo yum install target/rpm/dhis-integration/RPMS/noarch/dhis-integration-1.0-1.noarch.rpm -y
